acb9fe19e54d8a6dbaa1d998a379486f32b4c2f2
liufengting <wo157121900@me.com>
2018-08-08 10:42:35 +0800

    fix bugs

---
+++ FTPageViewControllerDemo/FTPageViewControllerDemo/DetialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetialViewController.swift
+//  FTPageViewControllerDemo
+//
+//  Created by liufengting on 2018/8/8.
+//  Copyright © 2018年 liufengting. All rights reserved.
+//
+
+import UIKit
+
+class DetialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
